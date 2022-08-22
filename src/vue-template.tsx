e78880538c957eaf9a9fcd2d891f9e620fc8eeac
perseus2791 <perseus2791@users.noreply.github.com>
2022-08-22 20:02:05 +0200

    Update vue-template.tsx
    add appContext of current app

--- src/vue-template.tsx
+++ src/vue-template.tsx
@@ -1,28 +1,29 @@
 import { RevoGrid } from '@revolist/revogrid/dist/types/interfaces';
 import { VNode } from '@revolist/revogrid/dist/types/stencil-public-runtime';
-import { ComponentPublicInstance, createVNode, DefineComponent, render } from 'vue';
+import { ComponentPublicInstance, createVNode, DefineComponent, render, getCurrentInstance } from "vue";
 
 
 interface VueElement extends HTMLElement {
     _vnode?: ComponentPublicInstance<any>;
 }
 export const vueTemplateConstructor =
-    (vueConstructor: DefineComponent, e: HTMLElement|null, p: Record<string, any>) => {
+    (vueConstructor: DefineComponent, e: HTMLElement|null, p: Record<string, any>, appContext: Object | any) => {
     if (!el) {
-    return null;
+      return null;
     }
 
-    // let el: VueElement = e;
     // check, probably vue instance already inited
     let vNode = el._vnode;
     // if exists, return
     if (vNode) {
+        if (appContext) vNode.appContext = appContext;
         // if vue inited just update it's properties
         for (const k in p) {
             vNode.component.props[k] = p[k];
         }
     } else {
         vNode = createVNode(vueConstructor, p);
+        if (appContext) vNode.appContext = appContext;
         render(vNode, el);
     }
 
@@ -32,7 +33,9 @@ export const vueTemplateConstructor =
 };
 
 const vueTemplate = (vueConstructor: any) => {
-    return (h: RevoGrid.HyperFunc<VNode>, p: RevoGrid.ColumnDataSchemaModel) => <span ref={(el) => vueTemplateConstructor(vueConstructor, el as HTMLElement, p)}></span>;
+    const current = getCurrentInstance();
+    const appContext = current?.appContext;
+    return (h: RevoGrid.HyperFunc<VNode>, p: RevoGrid.ColumnDataSchemaModel) => <span ref={(el) => vueTemplateConstructor(vueConstructor, el as HTMLElement, p, appContext)}></span>;
 };
 
 export default vueTemplate;
